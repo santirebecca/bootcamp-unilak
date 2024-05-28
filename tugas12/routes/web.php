cf04c0239fb678211d78237219cb8487533d7bb8
santirebecca <santirebecca225@gmail.com>
2024-05-28 23:16:53 +0700

    tugas12

---
+++ tugas12/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class,'home']);
+
+Route::get('/register', [AuthController::class,'register']);
+
+Route::POST('/welcome', [AuthController::class,'welcome']);
+
+Route::get('/data-tables', function(){
+    return view ('page.data-tables');
+});
+
+Route::get('/table', function(){
+    return view ('page.table');
+});
