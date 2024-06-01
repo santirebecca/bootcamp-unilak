a9a4f4c621f68bfa92ba685cbda60ae04d19380d
santirebecca <santirebecca225@gmail.com>
2024-06-02 05:08:58 +0700

    tugas14crud

---
+++ tugas14/resources/views/cast/bio.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title')
+      Halaman Bio Cast  
+@endsection
+@section('content')
+<h1>{{$cast->nama}}</h1>
+    <p>{{$cast->umur}}</p>
+    <p>{{$cast->bio}}</p>
+
+    <a href="/cast" class="btn btn-sm btn-secondary">Kembali</a>
+@endsection
